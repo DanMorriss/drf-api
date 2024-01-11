0266608961cc1f8eb5690cc2955599d3bc358b4e
Dan Morriss <danielmorriss1@gmail.com>
2024-01-11 14:51:44 +0000

    Add commentList and CommentDetail views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Comment
+from .serializers import CommentSerializer, CommentDetailSerializer
 
-# Create your views here.
+
+class CommentList(generics.ListCreateAPIView):
+    # The ListCreateAPIView means we don's have to write get (List takes care of that) and post (Create) methods.
+    # Also the request is part of the object model by default so we dont need to pass it in manually.
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Comment.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
+    queryset = Comment.objects.all()
