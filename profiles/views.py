b54c2eb124866a594613c189d913b82e1a37d50c
Dan Morriss <danielmorriss1@gmail.com>
2024-01-14 11:07:15 +0000

    Add filerting to comments

--- profiles/views.py
+++ profiles/views.py
@@ -21,8 +21,9 @@ class ProfileList(generics.ListAPIView):
         filters.OrderingFilter,
         DjangoFilterBackend,
     ]
-    filterset_fields =[
+    filterset_fields = [
         'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
     ordering_fields = [
         'posts_count',
